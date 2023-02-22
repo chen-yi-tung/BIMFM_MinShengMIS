3f54b372e0bc42d1c6cf4691fad451d9725c77ad
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-02-22 12:33:33 +0800

    保養項目及報修單管理頁面

--- MinSheng_MIS/Controllers/MaintainItemManagementController.cs
+++ MinSheng_MIS/Controllers/MaintainItemManagementController.cs
@@ -13,5 +13,35 @@ namespace MinSheng_MIS.Controllers
         {
             return View();
         }
+
+        //保養項目管理
+        public ActionResult MaintainItem_Management()
+        {
+            return View();
+        }
+
+        //新增保養項目
+        public ActionResult MaintainItem_Create()
+        {
+            return View();
+        }
+
+        //查詢保養項目 (詳情)
+        public ActionResult MaintainItem_Read()
+        {
+            return View();
+        }
+
+        //編輯保養項目
+        public ActionResult MaintainItem_Edit()
+        {
+            return View();
+        }
+
+        //刪除保養項目
+        public ActionResult MaintainItem_Delete()
+        {
+            return View();
+        }
     }
 }
--- MinSheng_MIS/Controllers/Report_ManagementController.cs
+++ MinSheng_MIS/Controllers/Report_ManagementController.cs
@@ -13,5 +13,17 @@ namespace MinSheng_MIS.Controllers
         {
             return View();
         }
+
+        //報修管理
+        public ActionResult Report_Management()
+        {
+            return View();
+        }
+
+        //報修管理詳情
+        public ActionResult Report_Read()
+        {
+            return View();
+        }
     }
 }
