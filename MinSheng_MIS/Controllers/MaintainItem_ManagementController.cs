c505dd925405f8e04697a442110bfbd46403839f
Yuti <yuti126593@gmail.com>
2023-03-01 12:18:22 +0800

    新增保養項目管理_檢索欄位的下拉選單

--- MinSheng_MIS/Controllers/MaintainItem_ManagementController.cs
+++ MinSheng_MIS/Controllers/MaintainItem_ManagementController.cs
@@ -3,15 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MinSheng_MIS.Models;
+using Newtonsoft.Json;
 
 namespace MinSheng_MIS.Controllers
 {
     public class MaintainItem_ManagementController : Controller
     {
+        Bimfm_MinSheng_MISEntities db = new Bimfm_MinSheng_MISEntities();
+
         #region 保養項目管理
         public ActionResult Management()
         {
-            return View();
+            MaintainItemViewModel maintainItemViewModel = new MaintainItemViewModel();
+
+            #region 下拉選單列出所有系統別
+            var SystemList = db.EquipmentInfo
+                .GroupBy(s => s.System, (key, items) => new SystemItems
+                { SystemName = key })
+                .ToList();
+
+            if (SystemList == null)
+            {
+                return HttpNotFound();
+            }
+
+            maintainItemViewModel.SystemItemList = SystemList;
+            #endregion
+
+            #region 下拉選單列出所有設備資訊
+            var MVM = new MaintainItemViewModel();
+            var equipmentList = db.EquipmentInfo
+                .OrderBy(e => e.ESN)
+                .ToList();
+            if (equipmentList == null)
+            {
+                return HttpNotFound();
+            }
+
+            if(equipmentList.Count() > 0)
+            {
+                MVM.EquipmentInfo = new List<EquipmentInfo>();
+                foreach (var item in equipmentList)
+                {
+                    MVM.EquipmentInfo.Add(new EquipmentInfo { ESN = item.ESN, System = item.System, SubSystem = item.SubSystem, EName = item.EName, Area = item.Area, Floor = item.Floor, Room = item.Room, RoomName = item.RoomName });
+                }
+            }
+
+            var equipmentItemJson = JsonConvert.SerializeObject(MVM, Formatting.Indented);
+
+            maintainItemViewModel.EquipmentInfoList = equipmentItemJson;
+            #endregion
+
+
+            #region 檢索
+
+
+            #endregion
+            return View(maintainItemViewModel);
         }
         #endregion
 
---
+++ MinSheng_MIS/Models/MaintainItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinSheng_MIS.Models
+{
+    public class MaintainItemViewModel
+    {
+        public List<SystemItems> SystemItemList { get; set; }
+
+        public List<EquipmentInfo> EquipmentInfo { get; set; }
+        public string EquipmentInfoList { get; set; }
+    }
+
+    public class SystemItems
+    {
+        public string SystemName { get; set; }
+    }
+}
