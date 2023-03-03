28622cf0d5d88f68f37415e65af60409bccc27c5
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-03-03 11:51:33 +0800

    123

---
+++ MinSheng_MIS/Models/ViewModels/SamplePathViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinSheng_MIS.Models.ViewModels
+{
+    public class SamplePathViewModel
+    {
+        public string PathTitle { get; set; }
+        public string Area { get; set; }
+        public string Floor { get; set; }
+
+        //查詢字串
+        public string QueryStr { get; set; }
+    }
+}
