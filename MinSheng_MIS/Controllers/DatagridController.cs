cc0657e65dd1106929e8607b2f4c24cc20d638d6
HaoYuShih <124142130+HaoYuShih@users.noreply.github.com>
2023-03-10 18:05:30 +0800

    20230310_1800

--- MinSheng_MIS/Controllers/DatagridController.cs
+++ MinSheng_MIS/Controllers/DatagridController.cs
@@ -6,15 +6,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MinSheng_MIS.Models.ViewModels;
+using MinSheng_MIS.Models;
 
 namespace MinSheng_MIS.Controllers
 {
     public class DatagridController : Controller
     {
+        MaintainRecordManagementModel MRMM = new MaintainRecordManagementModel();
+
         // GET: Datagrid
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            //ViewModel傳前端下拉選項
+            MaintainRecordManagementViewModel.Management management = new MaintainRecordManagementViewModel.Management()
+            { 
+                AreaList = MRMM.GetAreaList(),
+                FloorList= MRMM.GetFloorList(),
+                MaintainStateList= MRMM.GetMaintainStateList(),
+                ESNList = MRMM.GetESNList(),
+                ENameList= MRMM.GetENameList(),
+                MaintainUserIDList= MRMM.GetMaintainUserList(),
+                AuditUserIDList= MRMM.GetAuditUserList()
+            };
+            return View(management);
         }
         [HttpPost]
         public ActionResult Report_Management(FormCollection form)
---
+++ MinSheng_MIS/Models/MaintainRecordManagementModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Models
+{
+    public class MaintainRecordManagementModel
+    {
+        public List<SelectListItem> GetAreaList() 
+        { 
+            List<SelectListItem> list = new List<SelectListItem>();
+            return list;
+        }
+        public List<SelectListItem> GetFloorList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            return list;
+        }
+        public List<SelectListItem> GetMaintainStateList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            return list;
+        }
+        public List<SelectListItem> GetESNList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            return list;
+        }
+        public List<SelectListItem> GetENameList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            return list;
+        }
+        public List<SelectListItem> GetMaintainUserList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            return list;
+        }
+        public List<SelectListItem> GetAuditUserList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            return list;
+        }
+    }
+}
