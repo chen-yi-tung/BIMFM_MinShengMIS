1914c6ab8301d0117addce5ff94d652a905c539a
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-03-15 15:21:30 +0800

    update

--- MinSheng_MIS/Services/EquipmentMaintainForm_DataService.cs
+++ MinSheng_MIS/Services/EquipmentMaintainForm_DataService.cs
@@ -61,6 +61,8 @@ namespace MinSheng_MIS.Services
                               join x5 in db.Floor_Info on x3.FSN equals x5.FSN
                               join x6 in db.AreaInfo on x5.ASN equals x6.ASN
                               select new {  x1.FormItemState, x6.Area, x5.FloorName, x3.PropertyCode, x3.EName, x1.EMFISN, x4.MIName, x1.Unit, x1.Period, x1.LastTime, x1.Date};
+            
+            //
             #endregion
 
             //回傳JSON陣列
