45f4c98a6a385d6f3e51ad46f0c52f135118bf2c
HaoYuShih <124142130+HaoYuShih@users.noreply.github.com>
2023-03-16 17:14:33 +0800

    20230316_1700

--- MinSheng_MIS/Controllers/RepairRecord_ManagementController.cs
+++ MinSheng_MIS/Controllers/RepairRecord_ManagementController.cs
@@ -33,11 +33,24 @@ namespace MinSheng_MIS.Controllers
         #endregion
 
         #region 巡檢維修紀錄審核
-        public ActionResult Audit()
+        public ActionResult Audit(string id)
         {
+            ViewBag.id = id;
             return View();
         }
         #endregion
+
+        #region 
+        public ActionResult AuditBody(string id) //上面表格部分和詳情的資料都相同，只差下面編輯的部分要做
+        {
+            var repairRecord_Management_ReadViewModel = new RepairRecord_Management_ReadViewModel();
+
+            string result = repairRecord_Management_ReadViewModel.GetJsonForRead(id);
+            return Content(result, "application/json");
+        }
+        #endregion
+
+        //[補件]的顯示資料與[詳情]都相同除了沒有[維修資料]
 
         #region 巡檢維修紀錄補件
         public ActionResult Supplement()
