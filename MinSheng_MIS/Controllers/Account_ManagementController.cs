f4b6178c9fa647f1d482a5ccc8e3cf7b8b142cef
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-03-21 16:32:03 +0800

    新增帳號管理view

---
+++ MinSheng_MIS/Controllers/Account_ManagementController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Controllers
+{
+    public class Account_ManagementController : Controller
+    {
+        // GET: Account_Management
+        #region 帳號管理
+        public ActionResult Management()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 新增帳號
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 編輯帳號
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 刪除帳號
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
