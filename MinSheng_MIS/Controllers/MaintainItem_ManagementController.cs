912292e2bc879d3f806bd09ac0e43ef07c8800af
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-03-27 13:40:01 +0800

    保養項目編輯及刪除(若設備狀態為停用 則不顯示該設備)

--- MinSheng_MIS/Controllers/MaintainItem_ManagementController.cs
+++ MinSheng_MIS/Controllers/MaintainItem_ManagementController.cs
@@ -482,6 +482,8 @@ namespace MinSheng_MIS.Controllers
 
             var eqList = db.EquipmentMaintainItem
                 .Where(x => x.MISN == MISN)
+                .Join(db.EquipmentInfo, x1 => x1.ESN, x2 => x2.ESN, (x1, x2) => new { ESN = x1.ESN, Unit = x1.Unit, Period = x1.Period, EState = x2.EState })
+                .Where(x => x.EState != "3")
                 .ToList();
 
             if (eqList == null)
@@ -587,6 +589,8 @@ namespace MinSheng_MIS.Controllers
 
             var eqList = db.EquipmentMaintainItem
                 .Where(x => x.MISN == MISN)
+                .Join(db.EquipmentInfo, x1 => x1.ESN, x2 => x2.ESN, (x1, x2) => new { ESN = x1.ESN, Unit = x1.Unit, Period = x1.Period, EState = x2.EState })
+                .Where(x => x.EState != "3")
                 .ToList();
 
             if (eqList == null)
