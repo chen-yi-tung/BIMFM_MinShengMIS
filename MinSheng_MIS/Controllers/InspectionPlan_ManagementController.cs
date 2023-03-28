75c78008f9ce2f9b0a22d55eb142aa63258f87fe
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-03-28 09:57:20 +0800

    巡檢計畫 完成新增/刪除定期保養單

--- MinSheng_MIS/Controllers/InspectionPlan_ManagementController.cs
+++ MinSheng_MIS/Controllers/InspectionPlan_ManagementController.cs
@@ -1,8 +1,12 @@
-﻿using MinSheng_MIS.Models;
+﻿using Microsoft.Ajax.Utilities;
+using MinSheng_MIS.Models;
 using MinSheng_MIS.Services;
+using MinSheng_MIS.Surfaces;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -33,6 +37,143 @@ namespace MinSheng_MIS.Controllers
         {
             return View();
         }
+        #endregion
+
+        #region 新增巡檢計畫-新增定期保養單
+        [HttpPost]
+        public ActionResult AddMaintainForm(List<String> EMFISN)
+        {
+            JArray ja = new JArray();
+            foreach (var item in EMFISN)
+            {
+                //變更狀態為保留中
+                var maintainform = db.EquipmentMaintainFormItem.Find(item);
+                switch (maintainform.FormItemState)
+                {
+                    case "1":
+                        maintainform.FormItemState = "9";
+                        break;
+                    case "5":
+                        maintainform.FormItemState = "10";
+                        break;
+                    case "8":
+                        maintainform.FormItemState = "11";
+                        break;
+                }
+                db.EquipmentMaintainFormItem.AddOrUpdate(maintainform);
+                db.SaveChanges();
+                //回傳的顯示資料
+                JObject itemObjects = new JObject();
+                //庫存狀態
+                var StockStatedic = Surface.StockState();
+                if (maintainform.StockState)
+                {
+                    itemObjects.Add("StockState", StockStatedic["1"]);
+                }
+                else
+                {
+                    itemObjects.Add("StockState", StockStatedic["0"]);
+                }
+                //保養項目狀態
+                if (!string.IsNullOrEmpty(maintainform.FormItemState))
+                {
+                    var dic = Surface.EquipmentMaintainFormItemState();
+                    itemObjects.Add("FormItemState", dic[maintainform.FormItemState]);
+                }
+                //保養單項目編號
+                itemObjects.Add("EMFISN", maintainform.EMFISN);
+                //週期
+                itemObjects.Add("Period", maintainform.Period);
+                //週期單位
+                itemObjects.Add("Unit", maintainform.Unit);
+                //上次保養
+                itemObjects.Add("LastTime", maintainform.LastTime);
+                //最近應保養
+                itemObjects.Add("Date", maintainform.Date);
+
+                var EMISN = maintainform.EMISN;
+                var ESN = db.EquipmentMaintainItem.Find(EMISN).ESN;
+                var MISN = db.EquipmentMaintainItem.Find(EMISN).MISN;
+                var Equipment = db.EquipmentInfo.Find(ESN);
+                //設備狀態
+                if (!string.IsNullOrEmpty(Equipment.EState))
+                {
+                    var dic = Surface.EState();
+                    itemObjects.Add("EState", dic[Equipment.EState]);
+                }
+                //棟別
+                if (!string.IsNullOrEmpty(Equipment.Area))
+                {
+                    itemObjects.Add("Area", Equipment.Area);
+                }
+                //樓層
+                if (!string.IsNullOrEmpty(Equipment.Floor))
+                {
+                    itemObjects.Add("Floor", Equipment.Floor);
+                }
+                //設備編號
+                if (!string.IsNullOrEmpty(ESN))
+                {
+                    itemObjects.Add("ESN", ESN);
+                }
+                //設備名稱
+                if (!string.IsNullOrEmpty(Equipment.EName))
+                {
+                    itemObjects.Add("EName", Equipment.EName);
+                }
+                var maintainitem = db.MaintainItem.Find(MISN);
+                //保養項目
+                if (!string.IsNullOrEmpty(maintainitem.MIName))
+                {
+                    itemObjects.Add("MIName", maintainitem.MIName);
+                }
+                ja.Add(itemObjects);
+            }
+
+            JObject jo = new JObject();
+            jo.Add("rows", ja);
+
+            string result = JsonConvert.SerializeObject(jo);
+            return Content(result, "application/json");
+        }
+        #endregion
+
+        #region 新增巡檢計畫-刪除定期保養單
+        [HttpPost]
+        public ActionResult DeleteMaintainForm(List<String> EMFISN)
+        {
+            foreach (var item in EMFISN)
+            {
+                //變更狀態為保留中
+                var maintainform = db.EquipmentMaintainFormItem.Find(item);
+                switch (maintainform.FormItemState)
+                {
+                    case "9":
+                        maintainform.FormItemState = "1";
+                        break;
+                    case "10":
+                        maintainform.FormItemState = "5";
+                        break;
+                    case "11":
+                        maintainform.FormItemState = "8";
+                        break;
+                }
+                db.EquipmentMaintainFormItem.AddOrUpdate(maintainform);
+                db.SaveChanges();
+            }
+
+            JObject jo = new JObject();
+            jo.Add("Succed", true);
+
+            string result = JsonConvert.SerializeObject(jo);
+            return Content(result, "application/json");
+        }
+        #endregion
+
+        #region 新增巡檢計畫-新增維修設備
+        #endregion
+
+        #region 新增巡檢計畫-刪除維修設備
         #endregion
 
         #region 巡檢計畫詳情
