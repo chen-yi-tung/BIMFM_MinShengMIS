12c767f3fd14eeeb85080a3611518ddcf5c53f4d
HaoYuShih <124142130+HaoYuShih@users.noreply.github.com>
2023-04-07 15:44:05 +0800

    202304071543

---
+++ MinSheng_MIS/Models/ViewModels/InspectionPlanViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinSheng_MIS.Models.ViewModels
+{
+    public class InspectionPlanViewModel
+    {
+        Bimfm_MinSheng_MISEntities db = new Bimfm_MinSheng_MISEntities();
+
+    }
+}
