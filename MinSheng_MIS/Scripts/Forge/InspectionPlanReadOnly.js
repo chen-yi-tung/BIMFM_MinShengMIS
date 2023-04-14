957e25ba543b7cacc4a77a3776c1a39b5b587631
JudyJry <48381319+JudyJry@users.noreply.github.com>
2023-04-14 09:39:49 +0800

    update

---
+++ MinSheng_MIS/Scripts/Forge/InspectionPlanReadOnly.js
@@ -0,0 +1,50 @@
+﻿var view = document.querySelector("#PathCanvas");
+var app;
+
+function initializeDrawer() {
+    ForgeDraw.setDrawSetting("point.contextMenu.button", [{
+        name: "詳細",
+        onClick: function (event, point) {
+            console.log(`point ${point.index} => 詳細`, point);
+            createPointDetailModal(point);
+        }
+    }]);
+    ForgeDraw.setDrawSetting("devicePoint.contextMenu.button", [{
+        name: "詳細",
+        onClick: function (event, point) {
+            console.log(`point ${point.index} => 詳細`, point);
+            createDevicePointDetailModal(point);
+        }
+    }]);
+
+    app = ForgeDraw.init(view, viewer, function () {
+        ForgeDraw.setControl(ForgeDraw.Control.READONLY);
+        let pathData = JSON.parse(sessionStorage.getItem(`P1_pathData`))
+
+        createBeacons(pathData.PathSample.Beacon);
+        createDevices(1);
+        createLinePath(pathData.PathSampleRecord);
+        updatePathDisplay(pathData.PathSampleOrder, 1);
+
+        addTooltip(true);
+    });
+}
+
+window.addEventListener('load', function () {
+    pathGroup.setSetting({
+        startButton: {
+            id: "path-search",
+            icon: "icon-search",
+            tooltip: "查看此路線",
+            onClick: (e) => {
+                let pathID = $(e.target).closest(".sample-path-group").attr("data-path-id");
+                let pathData = JSON.parse(sessionStorage.getItem(`P${pathID}_pathData`))
+                $("#current-path-title").val(pathData.PathSample.PathTitle);
+                loadModel(
+                    window.location.origin + pathData.PathSample.BIMPath,
+                    pathID,
+                    loadPath)
+            }
+        }
+    })
+})
