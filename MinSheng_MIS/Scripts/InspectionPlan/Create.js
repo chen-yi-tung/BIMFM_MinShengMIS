658289e90420fd056e8efa52d5af5c6e1f9f4e48
JudyJry <48381319+JudyJry@users.noreply.github.com>
2023-04-17 10:54:25 +0800

    Update Create.js

--- MinSheng_MIS/Scripts/InspectionPlan/Create.js
+++ MinSheng_MIS/Scripts/InspectionPlan/Create.js
@@ -94,6 +94,30 @@ function addButtonEvent() {
 }
 
 function getCreateSaveData() {
+
+    let IPName = $("#IPName").val(),
+        PlanCreateUserID = $("#NavbarUserID").val(),
+        PlanDate = $("#PlanDate").val(),
+        Shift = $("#Shift").val(),
+        UserID = $("#UserID").tagbox('getValues')
+
+    if (!IPName) {
+        dialogError("請輸入巡檢計畫名稱！")
+        return;
+    }
+    if (!PlanDate) {
+        dialogError("請輸入巡檢日期！")
+        return;
+    }
+    if (!Shift) {
+        dialogError("請選擇巡檢班別！")
+        return;
+    }
+    if (UserID.length === 0) {
+        dialogError("請選擇巡檢人員！")
+        return;
+    }
+
     let MaintainEquipment = $("#Maintain-datagrid").parent().hasClass("d-none") ? null : $("#Maintain-datagrid").datagrid('getRows').map(e => e.EMFISN);
     let MaintainUserID = $("#MaintainUserID").val();
 
@@ -118,22 +142,33 @@ function getCreateSaveData() {
 
     for (const e of InspectionPlanPaths) {
         if (e.error && e.error === "order") {
-            dialogError(`計畫中設備未排入巡檢路線 ${e.pathTitle} 中！`)
+            if (e.equip.length > 0) {
+                dialogError(`設備${e.equip.join("、")}<br>未排入巡檢路線 ${e.pathTitle} 中！`)
+            }
+            else if (e.order.length > 0) {
+                dialogError(`${e.order.join("、")}設備不在此巡檢計畫中！`)
+            }
             return;
         }
     }
 
-    if (!checkAllAreaFloor(InspectionPlanPaths)) {
-        dialogError(`巡檢路線中無計畫中設備的棟別樓層！`)
+    let UnusedEquip = findUnusedEquip(InspectionPlanPaths)
+    if (UnusedEquip.length !== 0) {
+        let ueESN = [...new Set(UnusedEquip.map(e => e.ESN))];
+        dialogError(`
+        以下計畫中設備未排入巡檢路線中！<br>
+        ${ueESN.join("、")}<br>
+        請新增指定設備對應棟別樓層的巡檢路線！
+        `)
         return;
     }
 
     let data = {
-        IPName: $("#IPName").val(),
-        PlanCreateUserID: $("#NavbarUserID").val(),
-        PlanDate: $("#PlanDate").val(),
-        Shift: $("#Shift").val(),
-        UserID: $("#UserID").tagbox('getValues'),
+        IPName: IPName,
+        PlanCreateUserID: PlanCreateUserID,
+        PlanDate: PlanDate,
+        Shift: Shift,
+        UserID: UserID,
         MaintainUserID: MaintainUserID,
         RepairUserID: RepairUserID,
         MaintainEquipment: MaintainEquipment,
@@ -143,35 +178,37 @@ function getCreateSaveData() {
 
     console.log(data);
 
-    //return data;
+    return data;
 
     function getPathDatas() {
         return $(".sample-path-group[data-path-id]").toArray().map((e) => {
             let pathID = $(e).attr('data-path-id');
             let pathData = JSON.parse(sessionStorage.getItem(`P${pathID}_pathData`));
-            if (!checkOrder(pathData)) {
+            let check = checkOrder(pathData)
+            if (!check.result.every(e => e)) {
                 return {
                     error: "order",
                     pathID: pathID,
-                    pathTitle: pathData.PathSample.PathTitle
+                    pathTitle: pathData.PathSample.PathTitle,
+                    equip: check.equip,
+                    order: check.order,
+                    result: check.result
                 }
             }
             return pathData;
         })
     }
-    function checkAllAreaFloor(data) {
-        for (const e of getEquip()) {
-            for (const d of data) {
-                if (d.error) {
-                    return false;
-                }
-                if (e.ASN !== d.PathSample.ASN || e.FSN !== d.PathSample.FSN) {
-                    return false;
-                }
-            }
-        }
+    function findUnusedEquip(data) {
+        let equip = getEquip();
+        let result = equip.filter((e, i, arr) => {
+            return data.findIndex(d => {
+                if (d.error) return false
+                return e.ASN == d.PathSample.ASN || e.FSN === d.PathSample.FSN
+            }) === -1
+        })
+        console.log("findUnusedEquip result:", result)
 
-        return true;
+        return result;
 
         function getEquip() {
             return [...getDgRows(maintainDG_Controller), ...getDgRows(repairDG_Controller)]
@@ -189,20 +226,18 @@ function getCreateSaveData() {
             result = check(equip, order);
         }
 
-        console.log("checkOrder:", {
+        return {
             equip: equip,
             order: order,
             result: result
-        });
-
-        return result.every((e) => e);
+        };
 
         function getEquipData() {
             return [...new Set([...getESNs(maintainDG_Controller), ...getESNs(repairDG_Controller)])];
         }
         function getESNs(dgc) {
             return getDgRows(dgc).filter(e => {
-                return e.ASN === data.PathSample.ASN && e.FSN === data.PathSample.FSN
+                return e.ASN == data.PathSample.ASN && e.FSN === data.PathSample.FSN
             }).map(e => e.ESN)
         }
         function check(a, b) {
