112ff3805763826a9f46f6f6a729284e677678b3
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-04-18 17:34:11 +0800

    add Manufacturer Info View

---
+++ MinSheng_MIS/Controllers/ManufacturerInfo_ManagementController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Controllers
+{
+    public class ManufacturerInfo_ManagementController : Controller
+    {
+        // GET: ManufacturerInfo_Management
+        #region 廠商管理
+        public ActionResult Management()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 新增廠商
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 廠商詳情
+        public ActionResult Read()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 編輯廠商
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
