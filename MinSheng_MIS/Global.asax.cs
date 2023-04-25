c738cc0ac0fa8c2665716c98358bbc71e426c226
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-04-25 12:23:35 +0800

    change timer event to global.asax.cs

--- MinSheng_MIS/Global.asax.cs
+++ MinSheng_MIS/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MinSheng_MIS.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,33 @@ namespace MinSheng_MIS
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            #region 檢查是否需產生設備保養單項目
+            Check_EquipmentFormItem c = new Check_EquipmentFormItem();
+            c.CheckEquipmentFormItem();
+            #endregion
+
+            //#region 檢查巡檢計畫是否過期還沒開始巡檢
+            //Check_InspectionPlan checkplan = new Check_InspectionPlan();
+            //checkplan.CheckInspectionPlan();
+            //#endregion
+
+            System.Timers.Timer Wtimer = new System.Timers.Timer(3600000);//執行任務的週期 //小時3600000 1000
+            Wtimer.Elapsed += new System.Timers.ElapsedEventHandler(Wtimer_Elapsed);
+            Wtimer.Enabled = true;
+            Wtimer.AutoReset = true;
+        }
+        async void Wtimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            #region 檢查是否需產生設備保養單項目
+            Check_EquipmentFormItem c = new Check_EquipmentFormItem();
+            c.CheckEquipmentFormItem();
+            #endregion
+            //#region 檢查巡檢計畫是否過期還沒開始巡檢
+            //Check_InspectionPlan checkplan = new Check_InspectionPlan();
+            //checkplan.CheckInspectionPlan();
+            //#endregion
+
         }
     }
 }
