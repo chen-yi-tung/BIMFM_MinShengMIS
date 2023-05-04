4a7da1d59c66dab453d0194814aca746ea68f2fa
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2023-05-04 09:44:15 +0800

    新增入庫、庫存、領用申請、出庫相關view

---
+++ MinSheng_MIS/Controllers/StockIn_ManagementController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Controllers
+{
+    public class StockIn_ManagementController : Controller
+    {
+        // GET: StockIn_Management
+        #region 入庫管理
+        public ActionResult Management()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 新增入庫
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 入庫詳情
+        public ActionResult Read()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ MinSheng_MIS/Controllers/StoresRequisition_ManagementController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Controllers
+{
+    public class StoresRequisition_ManagementController : Controller
+    {
+        // GET: StoresRequisition_Management
+        #region 領用申請管理
+        public ActionResult Management()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 領用申請
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 領用申請詳情
+        public ActionResult Read()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 編輯領用申請
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 審核領用申請
+        public ActionResult Audit()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
