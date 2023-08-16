daf303381642f21804afbeed98e8510f8528ee64
JudyJry <48381319+JudyJry@users.noreply.github.com>
2023-08-16 18:00:52 +0800

    update

---
+++ MinSheng_MIS/Controllers/BIMController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Controllers
+{
+	public class BIMController : Controller
+	{
+		// GET: BIM
+		public ActionResult ModelView(string id)
+		{
+			ViewBag.id = id;
+			return View();
+		}
+	}
+}
