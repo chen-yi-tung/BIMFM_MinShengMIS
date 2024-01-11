05990d70926c9d3edb25a2815640587439d18817
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2024-01-11 16:23:38 +0800

    警示訊息 詳情 完成

--- MinSheng_MIS/Controllers/WarningMessage_ManagementController.cs
+++ MinSheng_MIS/Controllers/WarningMessage_ManagementController.cs
@@ -1,4 +1,7 @@
 ﻿using MinSheng_MIS.Models;
+using MinSheng_MIS.Models.ViewModels;
+using MinSheng_MIS.Surfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +24,53 @@ namespace MinSheng_MIS.Controllers
 		{
 			return View();
 		}
-		public ActionResult Read()
+        #region 警示訊息詳情
+        public ActionResult Read(string id)
 		{
-			return View();
+			ViewBag.id = id;
+
+            return View();
 		}
-	}
+		[HttpGet]
+		public ActionResult GetWarningMessageInfo(string WMSN)
+		{
+			var messageinfo = db.WarningMessage.Find(WMSN);
+			var WMTypedic = Surface.WMType();
+			var WMState = Surface.WMState();
+			var FloorName = db.Floor_Info.Find(messageinfo.FSN).FloorName.ToString();
+			var Area = db.Floor_Info.Find(messageinfo.FSN).AreaInfo.Area.ToString();
+
+            WarningMessageViewModel warningMessage = new WarningMessageViewModel();
+			warningMessage.WMSN = WMSN;
+			warningMessage.WMType = WMTypedic[messageinfo.WMType];
+			warningMessage.WMState = WMState[messageinfo.WMState];
+			warningMessage.TimeOfOccurrence = messageinfo.TimeOfOccurrence.ToString("yyyy/MM/dd HH:mm:ss");
+			warningMessage.Location = Area + " " + FloorName;
+			warningMessage.Message = messageinfo.Message;
+
+			//填報紀錄
+			var records = db.WarningMessageFillinRecord.Where(x => x.WMSN == WMSN).ToList();
+			if (records.Count() > 0)
+			{
+				foreach(var record in records)
+				{
+					WarningMessageFillinRecordViewModel r = new WarningMessageFillinRecordViewModel();
+					r.FillinDateTime = record.FillinDateTime.ToString("yyyy/MM/dd HH:mm:ss");
+					r.MyName = db.AspNetUsers.Where(x => x.UserName == record.FillinUserName).FirstOrDefault().MyName.ToString();
+					r.FillinState = WMState[record.FillinState];
+					r.Memo = record.Memo;
+					warningMessage.records.Add(r);
+                }
+			}
+			else
+			{
+				warningMessage.records = null;
+
+            }
+
+            string result = JsonConvert.SerializeObject(warningMessage);
+            return Content(result, "application/json");
+        }
+        #endregion
+    }
 }
---
+++ MinSheng_MIS/Models/ViewModels/WarningMessage_ManagementViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinSheng_MIS.Models.ViewModels
+{
+    public class WarningMessageViewModel
+    {
+        public string WMSN { get; set; } //警示訊息編號
+        public string WMType { get; set; } //事件等級
+        public string WMState { get; set; } //事件處理狀況
+        public string TimeOfOccurrence { get; set; } //發生時間
+        public string Location { get; set; } //發生地點
+        public string Message { get; set; } //事件內容
+        public List<WarningMessageFillinRecordViewModel> records { get; set; } //填報紀錄
+    }
+    public class WarningMessageFillinRecordViewModel
+    {
+        public string FillinDateTime { get; set; } //填報時間
+        public string MyName { get; set; } //填報人員
+        public string FillinState { get; set; } //事件處理狀況
+        public string Memo { get; set; } //備註
+    }
+}
