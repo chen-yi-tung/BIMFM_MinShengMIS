b4af08e8bcb83ee5c44bb4a459716f18d549f376
kuankuanli <111273868+kuankuanli@users.noreply.github.com>
2024-10-24 15:19:11 +0800

    資產管理、新增設備 切版

---
+++ MinSheng_MIS/Controllers/EquipmentInfo_ManagementController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Controllers
+{
+    public class EquipmentInfo_ManagementController : Controller
+    {
+        #region 資產管理
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 新增 設備
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 編輯 設備
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 設備 詳情
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 刪除 設備
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
