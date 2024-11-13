7dbd256f8592a0db27a7b4121cba894374b63bd5
kuankuanli <111273868+kuankuanli@users.noreply.github.com>
2024-11-13 14:01:00 +0800

    定期保養管理、報修管理 切版完成

---
+++ MinSheng_MIS/Controllers/Maintain_ManagementController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Controllers
+{
+    public class Maintain_ManagementController : Controller
+    {
+        #region 定期保養單管理
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 定期保養單 審核
+        public ActionResult Review()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 定期保養單 詳情
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ MinSheng_MIS/Controllers/Repair_ManagementController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinSheng_MIS.Controllers
+{
+    public class Repair_ManagementController : Controller
+    {
+        #region 報修管理
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 新增 報修單
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 報修單 審核
+        public ActionResult Review()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 報修單 詳情
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
