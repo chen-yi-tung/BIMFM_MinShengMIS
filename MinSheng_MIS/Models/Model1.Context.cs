de5a86a9700b788e226e6477c98ab26b41c5665d
ChenYiTung <57558829+bobo311@users.noreply.github.com>
2024-12-24 10:38:28 +0800

    Update Entity Model

--- MinSheng_MIS/Models/Model1.Context.cs
+++ MinSheng_MIS/Models/Model1.Context.cs
@@ -61,6 +61,7 @@ namespace MinSheng_MIS.Models
         public virtual DbSet<InspectionPlan_Member> InspectionPlan_Member { get; set; }
         public virtual DbSet<InspectionPlan_RFIDOrder> InspectionPlan_RFIDOrder { get; set; }
         public virtual DbSet<InspectionPlan_Time> InspectionPlan_Time { get; set; }
+        public virtual DbSet<InspectionTrack> InspectionTrack { get; set; }
         public virtual DbSet<LaboratoryMaintenance> LaboratoryMaintenance { get; set; }
         public virtual DbSet<ManufacturerInfo> ManufacturerInfo { get; set; }
         public virtual DbSet<MeetingMinutes> MeetingMinutes { get; set; }
@@ -86,6 +87,5 @@ namespace MinSheng_MIS.Models
         public virtual DbSet<WarningMessage> WarningMessage { get; set; }
         public virtual DbSet<WarningMessageFillinRecord> WarningMessageFillinRecord { get; set; }
         public virtual DbSet<WatchPairing> WatchPairing { get; set; }
-        public virtual DbSet<InspectionTrack> InspectionTrack { get; set; }
     }
 }
