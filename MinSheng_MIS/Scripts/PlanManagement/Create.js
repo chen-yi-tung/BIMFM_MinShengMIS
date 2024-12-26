10cf8aed64044dba1c26f6b55b9b3aa331c75285
nyxxxxx <s0941212@gmail.com>
2024-12-26 10:19:27 +0800

    feat: [巡檢管理-工單管理-新增] 功能開發(除了drag、自動帶入巡檢模板)

--- MinSheng_MIS/Scripts/PlanManagement/Create.js
+++ MinSheng_MIS/Scripts/PlanManagement/Create.js
@@ -4,71 +4,21 @@ let DatagridEvent;
 
 let autoLinkDG_Controller
 
-async function addDropDownList() {
-    //await pushSelect("Shift", "/DropDownList/Shift");
-    //await pushSelect("MaintainUserID", "/DropDownList/AllMyName");
-    //await pushSelect("RepairUserID", "/DropDownList/AllMyName");
-
-    //const DMM = "DatagridModal-Maintain";
-    //await pushSelect(DMM + " #FormItemState", "/DropDownList/AddFormItemState");
-    //await pushSelect(DMM + " #EState", "/DropDownList/EState?url=AddToPlan");
-    //await pushSelect(DMM + " #StockState", "/DropDownList/StockState");
-
-    //const DMR = "DatagridModal-Repair";
-    //await pushSelect(DMR + " #ReportLevel", "/DropDownList/ReportLevel");
-    //await pushSelect(DMR + " #ReportState", "/DropDownList/Report_Management_Management_ReportFormState?url=CanAddToPlanReportState");
-    //await pushSelect(DMR + " #UserID", "/DropDownList/ReportUser");
-    //await pushSelect(DMR + " #StockState", "/DropDownList/StockState");
-
-    //await $('#UserID').tagbox({
-    //    url: "/DropDownList/AllMyName",
-    //    textField: 'Text',
-    //    valueField: 'Value',
-    //    hasDownArrow: true,
-    //    limitToList: true,
-    //    validateOnCreate: false
-    //});
-}
-
-async function addAreaFloorEvent() {
-//    let areaList = ["path-form", "sample-path-form", "DatagridModal-Maintain", "DatagridModal-Repair"];
-//    areaList.forEach((id) => {
-//        pushSelect(`${id} #ASN`, "/DropDownList/Area");
-//        let ASN = $(`#${id} #ASN`);
-
-//        ASN.on("change", async function () {
-
-//            if (ASN.val()) {
-//                $(`#${id} #Area`).val(ASN.children("option:selected").text());
-//            }
-//            else {
-//                $(`#${id} #Area`).val('');
-//            }
-
-//            await pushSelect(`${id} #FSN`, "/DropDownList/Floor" + `?ASN=${ASN.val()}`);
-//            $(`#${id} #FSN`).val('');
-//        });
-//    })
-
-//    let floorList = ["path-form", "sample-path-form", "DatagridModal-Maintain", "DatagridModal-Repair"];
-//    floorList.forEach((id) => {
-//        let FSN = $(`#${id} #FSN`);
-
-//        FSN.on("change", async function () {
-
-//            if (FSN.val()) {
-//                $(`#${id} #Floor`).val(FSN.children("option:selected").text());
-//            }
-//            else {
-//                $(`#${id} #Floor`).val('');
-//            }
-
-//            if (id == "sample-path-form") {
-//                await pushSelect(`${id} #PathTitle`, "/DropDownList/PathTitle" + `?FSN=${FSN.val()}`);
-//                $(`#${id} #PathTitle`).val('');
-//            }
-//        });
-//    })
+async function addDropDownList(selector) {
+    console.log("selector", selector);
+    await $(selector).tagbox({
+        url: "/DropDownList/AllMyName",
+        textField: 'Text',
+        valueField: 'Value',
+        hasDownArrow: true,
+        limitToList: true,
+        validateOnCreate: false,
+        tagStyler: function (value) {
+            if (value) {
+                return 'background:#5480CA; color:#FFF; padding: 4px; height: fit-content;';
+            }
+        }
+    });
 }
 
 function addButtonEvent() {
@@ -90,105 +40,37 @@ function addButtonEvent() {
     })
     $("#submit").click(function () {
         console.log("onclick submit")
-        checkNeedSaveCurrentPath(save);
+        save(getCreateSaveData());
+    })
+    $("#addItem").click(function () {
+        console.log("onclick addItem")
+        sampleTr.create();
     })
 }
 
-function getCreateSaveData(IPSN = undefined) {
-
-    let IPName = $("#IPName").val(),
-        PlanCreateUserID = $("#NavbarUserID").val(),
-        PlanDate = $("#PlanDate").val(),
-
-    if (!IPName) {
+function getCreateSaveData() {
+    let PlanName = $("#PlanName").val();
+    let PlanDate = $("#PlanDate").val();
+    if (!PlanName) {
         dialogError("請輸入工單名稱！")
         return;
     }
     if (!PlanDate) {
-        dialogError("請輸入工單日期！")
+        dialogError("請輸入工單日期名稱！")
         return;
     }
-
-    let MaintainEquipment = $("#Maintain-datagrid").parent().hasClass("d-none") ? null : $("#Maintain-datagrid").datagrid('getRows').map(e => e.EMFISN);
-    let MaintainUserID = $("#MaintainUserID").val();
-
-    if (MaintainEquipment !== null && MaintainUserID === '') {
-        dialogError("未選擇定期保養審核人員！")
+    if (!sampleTr.checkRequired()) {
+        dialogError("請至少新增一項巡檢路線！")
         return;
     }
-
-    let data = {
-        IPName: IPName,
-        PlanCreateUserID: PlanCreateUserID,
-        PlanDate: PlanDate,
-        Shift: Shift,
-        UserID: UserID,
-        MaintainUserID: MaintainUserID,
-        RepairUserID: RepairUserID,
-        MaintainEquipment: MaintainEquipment,
-        RepairEquipment: RepairEquipment,
-        InspectionPlanPaths: InspectionPlanPaths
+    if (!sampleTr.checkValidity()) {
+        dialogError("請檢查是否完成模板內容必填！")
+        return;
     }
-
-    if (IPSN !== '' || IPSN !== undefined || IPSN !== null) {
-        data.IPSN = IPSN
-    }
-
-    console.log(data);
-
-    return data;
-
-    function findUnusedEquip(data) {
-        let equip = getEquip();
-        let result = equip.filter((e, i, arr) => {
-            return data.findIndex(d => {
-                if (d.error) return false
-                return e.ASN == d.PathSample.ASN && e.FSN === d.PathSample.FSN
-            }) === -1
-        })
-        console.log("findUnusedEquip result:", result)
-
-        return result;
-
-        function getEquip() {
-            return [...getDgRows(autoLinkDG_Controller), ...getDgRows(templateDG_Controller)]
-        }
-    }
-    function checkOrder(data) {
-        let equip = getEquipData();
-        let beacon = data.PathSample.Beacon.map(e => e.deviceName);
-        let order = data.PathSampleOrder.filter(e => !beacon.includes(e));
-        let result;
-
-        if (equip.length > order.length) {
-            result = check(order, equip);
-        } else if (equip.length <= order.length) {
-            result = check(equip, order);
-        }
-
-        return {
-            equip: equip,
-            order: order,
-            result: result
-        };
-
-        function getEquipData() {
-            return [...new Set([...getESNs(autoLinkDG_Controller), ...getESNs(templateDG_Controller)])];
-        }
-        function getESNs(dgc) {
-            return getDgRows(dgc).filter(e => {
-                return e.ASN == data.PathSample.ASN && e.FSN === data.PathSample.FSN
-            }).map(e => e.ESN)
-        }
-        function check(a, b) {
-            return b.map(e => a.includes(e));
-        }
-    }
-    function getDgRows(dgc) {
-        if (dgc.edg.parent().hasClass('d-none')) {
-            return [];
-        }
-        return dgc.edg.datagrid('getRows');
+    return {
+        PlanName,
+        PlanDate,
+        TempalteItems: sampleTr.calc()
     }
     function dialogError(inner) {
         createDialogModal({ id: "DialogModal-Error", inner: inner })
--- MinSheng_MIS/Scripts/SampleSchedule_Management/Create.js
+++ MinSheng_MIS/Scripts/SampleSchedule_Management/Create.js
@@ -4,73 +4,6 @@ let DatagridEvent;
 
 let autoLinkDG_Controller
 
-async function addDropDownList() {
-    //await pushSelect("Shift", "/DropDownList/Shift");
-    //await pushSelect("MaintainUserID", "/DropDownList/AllMyName");
-    //await pushSelect("RepairUserID", "/DropDownList/AllMyName");
-
-    //const DMM = "DatagridModal-Maintain";
-    //await pushSelect(DMM + " #FormItemState", "/DropDownList/AddFormItemState");
-    //await pushSelect(DMM + " #EState", "/DropDownList/EState?url=AddToPlan");
-    //await pushSelect(DMM + " #StockState", "/DropDownList/StockState");
-
-    //const DMR = "DatagridModal-Repair";
-    //await pushSelect(DMR + " #ReportLevel", "/DropDownList/ReportLevel");
-    //await pushSelect(DMR + " #ReportState", "/DropDownList/Report_Management_Management_ReportFormState?url=CanAddToPlanReportState");
-    //await pushSelect(DMR + " #UserID", "/DropDownList/ReportUser");
-    //await pushSelect(DMR + " #StockState", "/DropDownList/StockState");
-
-    //await $('#UserID').tagbox({
-    //    url: "/DropDownList/AllMyName",
-    //    textField: 'Text',
-    //    valueField: 'Value',
-    //    hasDownArrow: true,
-    //    limitToList: true,
-    //    validateOnCreate: false
-    //});
-}
-
-async function addAreaFloorEvent() {
-//    let areaList = ["path-form", "sample-path-form", "DatagridModal-Maintain", "DatagridModal-Repair"];
-//    areaList.forEach((id) => {
-//        pushSelect(`${id} #ASN`, "/DropDownList/Area");
-//        let ASN = $(`#${id} #ASN`);
-
-//        ASN.on("change", async function () {
-
-//            if (ASN.val()) {
-//                $(`#${id} #Area`).val(ASN.children("option:selected").text());
-//            }
-//            else {
-//                $(`#${id} #Area`).val('');
-//            }
-
-//            await pushSelect(`${id} #FSN`, "/DropDownList/Floor" + `?ASN=${ASN.val()}`);
-//            $(`#${id} #FSN`).val('');
-//        });
-//    })
-
-//    let floorList = ["path-form", "sample-path-form", "DatagridModal-Maintain", "DatagridModal-Repair"];
-//    floorList.forEach((id) => {
-//        let FSN = $(`#${id} #FSN`);
-
-//        FSN.on("change", async function () {
-
-//            if (FSN.val()) {
-//                $(`#${id} #Floor`).val(FSN.children("option:selected").text());
-//            }
-//            else {
-//                $(`#${id} #Floor`).val('');
-//            }
-
-//            if (id == "sample-path-form") {
-//                await pushSelect(`${id} #PathTitle`, "/DropDownList/PathTitle" + `?FSN=${FSN.val()}`);
-//                $(`#${id} #PathTitle`).val('');
-//            }
-//        });
-//    })
-}
-
 function addButtonEvent() {
     $("#back").click(function () {
         history.back();
@@ -109,7 +42,6 @@ function getCreateSaveData() {
         dialogError("請檢查是否完成模板內容必填！")
         return;
     }
-    //console.log(data);
     return {
         TemplateName,
         TempalteItems: sampleTr.calc()
