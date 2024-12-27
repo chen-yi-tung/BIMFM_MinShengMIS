c942bc4dd51976875fe0eb30d256b1ccc1b2fdb7
Turbobon <85898697+Turbobon@users.noreply.github.com>
2024-12-27 15:53:40 +0800

    update 保養單審核人員名稱

--- MinSheng_MIS/Controllers/Maintain_ManagementController.cs
+++ MinSheng_MIS/Controllers/Maintain_ManagementController.cs
@@ -34,6 +34,11 @@ namespace MinSheng_MIS.Controllers
         #region 定期保養單 審核
         public ActionResult Review()
         {
+            string userName = HttpContext.User.Identity.Name;
+            using (Bimfm_MinSheng_MISEntities db = new Bimfm_MinSheng_MISEntities())
+            {
+                ViewBag.MyName = db.AspNetUsers.Where(a => a.UserName == userName).Select(a => a.MyName).FirstOrDefault();
+            }
             return View();
         }
         [HttpPost]
