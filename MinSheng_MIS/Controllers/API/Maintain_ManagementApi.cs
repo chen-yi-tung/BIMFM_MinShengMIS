832bb3b1339903a9c8d0103c07c45bfe41a98a17
Turbobon <85898697+Turbobon@users.noreply.github.com>
2024-12-27 12:15:49 +0800

    feat: APP保養單API

---
+++ MinSheng_MIS/Controllers/API/Maintain_ManagementApi.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Ajax.Utilities;
+using MinSheng_MIS.Models;
+using MinSheng_MIS.Models.ViewModels;
+using MinSheng_MIS.Services;
+using MinSheng_MIS.Surfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace MinSheng_MIS.Controllers.API
+{
+    #region 定期保養單 列表
+    public class MaintainListController : ApiController
+    {
+        private readonly Bimfm_MinSheng_MISEntities _db;
+        private readonly Maintain_ManagementService _maintainService;
+
+        public MaintainListController()
+        {
+            _db = new Bimfm_MinSheng_MISEntities();
+            _maintainService = new Maintain_ManagementService(_db);
+        }
+        public JsonResService<MaintainManagementApp_ListViewModel> Get(string Status = null)
+        {
+            JsonResService<MaintainManagementApp_ListViewModel> result = new JsonResService<MaintainManagementApp_ListViewModel>();
+            try
+            {
+                result = _maintainService.MaintainManagementApp_List(Status);
+            }
+            catch (MyCusResException ex)
+            {
+                result.AccessState = ResState.Failed;
+                result.ErrorMessage = ex.Message;
+            }
+            catch (Exception)
+            {
+                result.AccessState = ResState.Failed;
+                result.ErrorMessage = "系統異常!";
+            }
+            return result;
+        }
+    }
+    #endregion
+
+    #region 定期保養單 填報詳情
+    public class MaintainDetailController : ApiController
+    {
+        private readonly Bimfm_MinSheng_MISEntities _db;
+        private readonly Maintain_ManagementService _maintainService;
+
+        public MaintainDetailController()
+        {
+            _db = new Bimfm_MinSheng_MISEntities();
+            _maintainService = new Maintain_ManagementService(_db);
+        }
+        public JsonResService<MaintainManagementApp_Detail> Get(string EMFSN)
+        {
+            JsonResService<MaintainManagementApp_Detail> result = new JsonResService<MaintainManagementApp_Detail>();
+            try
+            {
+                result = _maintainService.MaintainManagementApp_Detail(EMFSN);
+            }
+            catch (MyCusResException ex)
+            {
+                result.AccessState = ResState.Failed;
+                result.ErrorMessage = ex.Message;
+            }
+            catch (Exception)
+            {
+                result.AccessState = ResState.Failed;
+                result.ErrorMessage = "系統異常!";
+            }
+            return result;
+        }
+    }
+    #endregion
+
+    #region 定期保養單 填報
+    public class MaintainReportController : ApiController
+    {
+        private readonly Bimfm_MinSheng_MISEntities _db;
+        private readonly Maintain_ManagementService _maintainService;
+
+        public MaintainReportController()
+        {
+            _db = new Bimfm_MinSheng_MISEntities();
+            _maintainService = new Maintain_ManagementService(_db);
+        }
+        public JsonResService<string> Post(MaintainManagementApp_Report datas)
+        {
+            JsonResService<string> result = new JsonResService<string>();
+            try
+            {
+                result = _maintainService.MaintainManagementApp_Report(datas);
+            }
+            catch (MyCusResException ex)
+            {
+                result.AccessState = ResState.Failed;
+                result.ErrorMessage = ex.Message;
+            }
+            catch (Exception)
+            {
+                result.AccessState = ResState.Failed;
+                result.ErrorMessage = "系統異常!";
+            }
+            return result;
+        }
+    }
+    #endregion
+}
--- MinSheng_MIS/Models/ViewModels/Maintain_ManagementViewModel.cs
+++ MinSheng_MIS/Models/ViewModels/Maintain_ManagementViewModel.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace MinSheng_MIS.Models.ViewModels
 {
+    #region 定期保養單 詳情
     public class Maintain_ManagementDetailViewModel
     {
         public string EMFSN { get; set; }
@@ -26,18 +29,69 @@ namespace MinSheng_MIS.Models.ViewModels
         public string AuditTime { get; set; }
         public string AuditReason { get; set; }
     }
+    #endregion
 
+    #region 定期保養單 派工
     public class Maintain_ManagementAssignmentViewModel
     {
         public List<string> EMFSN { get; set; }
         public DateTime NextMaintainDate { get; set; }
         public List<string> Maintainer { get; set; }
     }
+    #endregion
 
+    #region 定期保養單 審核
     public class Maintain_ManagementAuditViewModel
     {
         public string EMFSN { get; set; }
         public string AuditReason { get; set; }
         public string AuditResult { get; set; }
     }
+    #endregion
+
+    public class MaintainManagementApp_ListViewModel
+    {
+        public string TotalNum {  get; set; }
+        public string PendingNum { get; set; }
+        public string NotApprovedNum { get; set; }
+        public List<MaintainManagementApp_ListItem> MaintainFormLsit { get; set; }
+        public class MaintainManagementApp_ListItem
+        {
+            public List<string> RFIDList{ get; set; }
+            public string EMFSN { get; set; }
+            public string Status { get; set; }
+            public string EName { get; set; }
+            public string NO { get; set; }
+            public string EState { get; set; }
+            public string Area { get; set; }
+            public string FloorName { get; set; }
+            public string DispatcherTime { get; set; }
+            public string NextMaintainDate { get; set; }
+        }
+    }
+
+    public class MaintainManagementApp_Detail
+    {
+        public string EMFSN { get; set; }
+        public string EName { get; set; }
+        public string NO { get; set; }
+        public string Area { get; set; }
+        public string FloorName { get; set; }
+        public string ESN { get; set; }
+        public string MaintainName { get; set; }
+        public string Period { get; set; }
+        public string LastMaintainDate { get; set; }
+        
+        // 審核內容
+        public bool? IsAudited { get; set; }
+        public string AuditReason { get; set; }
+    }
+
+    public class MaintainManagementApp_Report
+    {
+        public string EMFSN { get; set; }
+        public string ReportContent { get; set; }
+        public string UserName { get; set; }
+    }
+
 }
