26c95955da5b3255fec435f7aacb6e10e32d032e
Christine0120 <116532612+Christine0120@users.noreply.github.com>
2024-12-27 10:12:07 +0800

    APP巡檢模組API完成

--- MinSheng_MIS/Services/InspectionPlan_ManagementService.cs
+++ MinSheng_MIS/Services/InspectionPlan_ManagementService.cs
@@ -118,48 +118,57 @@ namespace MinSheng_MIS.Services
                         var TSN = _db.EquipmentInfo.Find(esn).TSN.ToString();
                         //建立巡檢設備檢查項目
                         var CheckItems = _db.Template_CheckItem.Where(x => x.TSN == TSN).OrderBy(x => x.CISN).ToList();
+                        var lastcheckItemId = _db.InspectionPlan_EquipmentCheckItem.Where(x => x.IPESN == plan_Equipment.IPESN).OrderByDescending(x => x.Id).FirstOrDefault()?.Id ?? (plan_Equipment.IPESN + "000");
+                        var InspectionPlan_EquipmentCheckItems = new List<InspectionPlan_EquipmentCheckItem>();
                         foreach (var checkItem in CheckItems)
                         {
                             InspectionPlan_EquipmentCheckItem inspectionPlan_EquipmentCheckItem = new InspectionPlan_EquipmentCheckItem();
-                            var lastId = _db.InspectionPlan_EquipmentCheckItem.Where(x => x.IPESN == plan_Equipment.IPESN).OrderByDescending(x => x.Id).FirstOrDefault()?.Id ?? (plan_Equipment.IPESN + "000");
-                            inspectionPlan_EquipmentCheckItem.Id = ComFunc.CreateNextID(plan_Equipment.IPESN+"%{3}", lastId);
+                            inspectionPlan_EquipmentCheckItem.Id = ComFunc.CreateNextID(plan_Equipment.IPESN+"%{3}", lastcheckItemId);
                             inspectionPlan_EquipmentCheckItem.IPESN = plan_Equipment.IPESN;
                             inspectionPlan_EquipmentCheckItem.CheckItemName = checkItem.CheckItemName;
-                            _db.InspectionPlan_EquipmentCheckItem.AddOrUpdate(inspectionPlan_EquipmentCheckItem);
-                            _db.SaveChanges();
+                            InspectionPlan_EquipmentCheckItems.Add(inspectionPlan_EquipmentCheckItem);
+                            lastcheckItemId = inspectionPlan_EquipmentCheckItem.Id;
                         }
+                        _db.InspectionPlan_EquipmentCheckItem.AddOrUpdate(InspectionPlan_EquipmentCheckItems.ToArray());
+                        _db.SaveChanges();
                         //建立巡檢設備填報項目
                         var ReportingItems = _db.Template_ReportingItem.Where(x => x.TSN == TSN).OrderBy(x => x.RISN).ToList();
+                        var lastReportingItemId = _db.InspectionPlan_EquipmentReportingItem.Where(x => x.IPESN == plan_Equipment.IPESN).OrderByDescending(x => x.Id).FirstOrDefault()?.Id ?? (plan_Equipment.IPESN + "000");
+                        var InspectionPlan_EquipmentReportingItems = new List<InspectionPlan_EquipmentReportingItem>();
                         foreach (var ReportingItem in ReportingItems)
                         {
                             InspectionPlan_EquipmentReportingItem inspectionPlan_EquipmentReportingItem = new InspectionPlan_EquipmentReportingItem();
-                            var lastId = _db.InspectionPlan_EquipmentReportingItem.Where(x => x.IPESN == plan_Equipment.IPESN).OrderByDescending(x => x.Id).FirstOrDefault()?.Id ?? (plan_Equipment.IPESN + "000");
-                            inspectionPlan_EquipmentReportingItem.Id = ComFunc.CreateNextID(plan_Equipment.IPESN+"%{3}", lastId);
+                            inspectionPlan_EquipmentReportingItem.Id = ComFunc.CreateNextID(plan_Equipment.IPESN+"%{3}", lastReportingItemId);
                             inspectionPlan_EquipmentReportingItem.IPESN = plan_Equipment.IPESN;
                             inspectionPlan_EquipmentReportingItem.ReportValue = ReportingItem.ReportingItemName;
                             inspectionPlan_EquipmentReportingItem.Unit = ReportingItem.Unit;
-                            _db.InspectionPlan_EquipmentReportingItem.AddOrUpdate(inspectionPlan_EquipmentReportingItem);
-                            _db.SaveChanges();
+                            InspectionPlan_EquipmentReportingItems.Add(inspectionPlan_EquipmentReportingItem);
+                            lastReportingItemId = inspectionPlan_EquipmentReportingItem.Id;
                         }
+                        _db.InspectionPlan_EquipmentReportingItem.AddOrUpdate(InspectionPlan_EquipmentReportingItems.ToArray());
+                        _db.SaveChanges();
                     }
                     //新增巡檢RFID順序
                     var RFIDs = from x1 in _db.InspectionDefaultOrder
                                 where x1.PlanPathSN == PlanPathSN
                                 orderby x1.DefaultOrder
-                                join x2 in _db.RFID on x1.RFIDInternalCode equals x2.RFIDExternalCode
+                                join x2 in _db.RFID on x1.RFIDInternalCode equals x2.RFIDInternalCode
                                 select new { x1.RFIDInternalCode, x2.ESN };
-                    foreach(var RFID in RFIDs)
+                    var InspectionPlan_RFIDOrders = new List<InspectionPlan_RFIDOrder>();
+                    var lastInspectionOrder = _db.InspectionPlan_RFIDOrder.Where(x => x.IPTSN == IPTSN).OrderByDescending(x => x.InspectionOrder).FirstOrDefault()?.InspectionOrder ?? (IPTSN + "00000");
+                    foreach (var RFID in RFIDs)
                     {
                         InspectionPlan_RFIDOrder inspectionPlan_RFIDOrder = new InspectionPlan_RFIDOrder();
-                        var lastInspectionOrder = _db.InspectionPlan_RFIDOrder.Where(x => x.IPTSN == IPTSN).OrderByDescending(x => x.InspectionOrder).FirstOrDefault()?.InspectionOrder ?? (IPTSN + "00000");
                         inspectionPlan_RFIDOrder.InspectionOrder = ComFunc.CreateNextID(IPTSN+"%{5}", lastInspectionOrder);
                         inspectionPlan_RFIDOrder.IPTSN = IPTSN;
                         inspectionPlan_RFIDOrder.IPESN = IPTSN + RFID.ESN;
                         inspectionPlan_RFIDOrder.RFIDInternalCode = RFID.RFIDInternalCode;
                         inspectionPlan_RFIDOrder.Status = "1";
-                        _db.InspectionPlan_RFIDOrder.AddOrUpdate(inspectionPlan_RFIDOrder);
-                        _db.SaveChanges();
+                        InspectionPlan_RFIDOrders.Add(inspectionPlan_RFIDOrder);
+                        lastInspectionOrder = inspectionPlan_RFIDOrder.InspectionOrder;
                     }
+                    _db.InspectionPlan_RFIDOrder.AddOrUpdate(InspectionPlan_RFIDOrders.ToArray());
+                    _db.SaveChanges();
                     //將巡檢時段計畫改為執行中
                     inspectionPlanTime.InspectionState = "2";
                     _db.InspectionPlan_Time.AddOrUpdate(inspectionPlanTime);
@@ -363,5 +372,6 @@ namespace MinSheng_MIS.Services
             }
         }
         #endregion
+
     }
 }
