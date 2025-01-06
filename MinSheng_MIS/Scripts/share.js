65740b8bf4e6d87a4638f2503492c9590ae1853b
kuankuanli <111273868+kuankuanli@users.noreply.github.com>
2025-01-06 15:08:07 +0800

    依權限顯示內容

--- MinSheng_MIS/Scripts/share.js
+++ MinSheng_MIS/Scripts/share.js
@@ -7,3 +7,27 @@ function addButtonEvent() {
         save();
     })
 }
+
+async function checkAuthority(userAuthorityUrl) {
+    try {
+        const res = await new Promise((resolve, reject) => {
+            $.ajax({
+                url: userAuthorityUrl,
+                type: "GET",
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                success: function (res) {
+                    resolve(res.Datas.Authority);
+                },
+                error: function (res) {
+                    reject(res);
+                },
+            });
+        });
+        //console.log("權限", res);
+        return res;
+    } catch (error) {
+        console.error("權限回傳失敗", error);
+        throw error;
+    }
+}
